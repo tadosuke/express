1d80521b4be2e235572161f4a988479539c3114d
tadosuke <tadosuke@gmail.com>
2025-02-02 11:15:50 +0900

    routes 以下のファイルをリネーム

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import apiRoutes from './routes/api.js';
+import apiRoutes from './routes/apiRoutes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
